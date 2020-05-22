1455c70781bc88a4ee52700ef8a94797d4e7000c
Aman Dhaka <46739076+amandhaka@users.noreply.github.com>
2020-05-22 20:29:56 +0530

    Create Sort Characters By Frequency.java

---
+++ Sort Characters By Frequency.java
@@ -0,0 +1,56 @@
+/*
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+*/
+
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        String result="";
+        PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));//Max-Heap with comparator interface
+        pq.addAll(map.keySet());
+        while(!pq.isEmpty()){
+            char current=pq.remove();
+            int count=map.get(current);
+            result+=Character.toString(current).repeat(count);
+        }
+        return result;
+    }
+}
