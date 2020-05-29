9a330eed58198218dcdc743e535178aa8a31f195
Aman Dhaka <46739076+amandhaka@users.noreply.github.com>
2020-05-29 23:09:11 +0530

    Create Course Schedule(Topological Sort).java

---
+++ Course Schedule(Topological Sort).java
@@ -0,0 +1,51 @@
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+Constraints:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+1 <= numCourses <= 10^5
+*/
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree=new int[numCourses];
+        //Storing inDegree of each node in graph
+        for(int i=0;i<prerequisites.length;i++){
+            inDegree[prerequisites[i][0]]++;
+        }
+        Stack<Integer> stack=new Stack<Integer>();
+        //Pushing elements which has 0 inDegree as they don't have any pre-requisite
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0)
+                stack.push(i);
+        }
+        int counter=0; //for maintaining the count of courses which can be taken till now
+        while(!stack.empty()){
+            int curr=stack.pop();
+            counter++;
+            for(int i=0;i<prerequisites.length;i++){
+                if(prerequisites[i][1]==curr){ 
+                    inDegree[prerequisites[i][0]]--;//decrement inDegree for node which is connect via curr node
+                    if(inDegree[prerequisites[i][0]]==0){
+                        stack.push(prerequisites[i][0]); //if the connected node now has no inDegree that means it can be stored in array as it is not dependent on any node now.
+                    }
+                }
+            }
+        }
+        return counter==numCourses;
+    }
+}
